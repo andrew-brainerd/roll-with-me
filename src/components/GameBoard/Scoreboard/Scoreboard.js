73ddda6bdd326aaab98ca9b2860aca737aadbaa0
andrew-brainerd <andrew.brainerd@llamasoft.com>
2020-06-23 23:18:15 -0400

    Add selected score

--- src/components/GameBoard/Scoreboard/Scoreboard.js
+++ src/components/GameBoard/Scoreboard/Scoreboard.js
@@ -1,69 +1,95 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { object } from 'prop-types';
 import styles from './Scoreboard.module.scss';
+import {
+  ONES,
+  TWOS,
+  THREES,
+  FOURS,
+  FIVES,
+  SIXES,
+  KIND3,
+  KIND4,
+  FULL_HOUSE,
+  SM_STRAIGHT,
+  LG_STRAIGHT,
+  KIND5,
+  CHANCE
+} from '../../../constants/game';
 
 const Scoreboard = ({ player1, currentScores }) => {
+  const [selectedScore, setSelectedScore] = useState({ slot: '' });
+
   const {
-    ones,
-    twos,
-    threes,
-    fours,
-    fives,
-    sixes,
-    kind3,
-    kind4,
-    fullHouse,
-    smStraight,
-    lgStraight,
-    kind5,
-    chance
+    [ONES]: ones,
+    [TWOS]: twos,
+    [THREES]: threes,
+    [FOURS]: fours,
+    [FIVES]: fives,
+    [SIXES]: sixes,
+    [KIND3]: kind3,
+    [KIND4]: kind4,
+    [FULL_HOUSE]: fullHouse,
+    [SM_STRAIGHT]: smStraight,
+    [LG_STRAIGHT]: lgStraight,
+    [KIND5]: kind5,
+    [CHANCE]: chance
   } = player1;
 
   const {
-    ones: availableOnes,
-    twos: availableTwos,
-    threes: availableThrees,
-    fours: availableFours,
-    fives: availableFives,
-    sixes: availableSixes,
-    kind3: availableKind3,
-    kind4: availableKind4,
-    fullHouse: availableFullHouse,
-    smStraight: availableSmStraight,
-    lgStraight: availableLgStraight,
-    kind5: availableKind5,
-    chance: availableChance
+    [ONES]: availableOnes,
+    [TWOS]: availableTwos,
+    [THREES]: availableThrees,
+    [FOURS]: availableFours,
+    [FIVES]: availableFives,
+    [SIXES]: availableSixes,
+    [KIND3]: availableKind3,
+    [KIND4]: availableKind4,
+    [FULL_HOUSE]: availableFullHouse,
+    [SM_STRAIGHT]: availableSmStraight,
+    [LG_STRAIGHT]: availableLgStraight,
+    [KIND5]: availableKind5,
+    [CHANCE]: availableChance
   } = currentScores;
+
+  console.log(selectedScore);
 
   return (
     <div className={styles.scoreboard}>
       <div className={styles.section}>
-        <div className={styles.scoreLine}>
+        <div className={styles.scoreLine} onClick={() => setSelectedScore({ slot: ONES, score: availableOnes })}>
           <div className={styles.scoreLabel}>Ones</div>
           <div className={styles.scoreValue}>{ones}</div>
-          <div className={styles.scoreAvailable}>{availableOnes}</div>
+          <div
+            className={[
+              styles.scoreAvailable,
+              selectedScore.slot === ONES ? styles.selected : ''
+            ].join(' ')}
+          >
+            {availableOnes}
+          </div>
         </div>
-        <div className={styles.scoreLine}>
+        <div className={styles.scoreLine} onClick={() => setSelectedScore({ slot: TWOS, score: availableTwos })}>
           <div className={styles.scoreLabel}>Twos</div>
           <div className={styles.scoreValue}>{twos}</div>
           <div className={styles.scoreAvailable}>{availableTwos}</div>
         </div>
-        <div className={styles.scoreLine}>
+        <div className={styles.scoreLine} onClick={() => setSelectedScore({ slot: THREES, score: availableThrees })}>
           <div className={styles.scoreLabel}>Threes</div>
           <div className={styles.scoreValue}>{threes}</div>
           <div className={styles.scoreAvailable}>{availableThrees}</div>
         </div>
-        <div className={styles.scoreLine}>
+        <div className={styles.scoreLine} onClick={() => setSelectedScore({ slot: FOURS, score: availableFours })}>
           <div className={styles.scoreLabel}>Fours</div>
           <div className={styles.scoreValue}>{fours}</div>
           <div className={styles.scoreAvailable}>{availableFours}</div>
         </div>
-        <div className={styles.scoreLine}>
+        <div className={styles.scoreLine} onClick={() => setSelectedScore({ slot: FIVES, score: availableFives })}>
           <div className={styles.scoreLabel}>Fives</div>
           <div className={styles.scoreValue}>{fives}</div>
           <div className={styles.scoreAvailable}>{availableFives}</div>
         </div>
-        <div className={styles.scoreLine}>
+        <div className={styles.scoreLine} onClick={() => setSelectedScore({ slot: SIXES, score: availableSixes })}>
           <div className={styles.scoreLabel}>Sixes</div>
           <div className={styles.scoreValue}>{sixes}</div>
           <div className={styles.scoreAvailable}>{availableSixes}</div>
--- src/constants/game.js
+++ src/constants/game.js
@@ -1,18 +1,32 @@
 export const PLAYER1 = 'player1';
 export const PLAYER2 = 'player2';
 
+export const ONES = 'ones';
+export const TWOS = 'twos';
+export const THREES = 'threes';
+export const FOURS = 'fours';
+export const FIVES = 'fives';
+export const SIXES = 'sixes';
+export const KIND3 = 'kind3';
+export const KIND4 = 'kind4';
+export const FULL_HOUSE = 'fullHouse';
+export const SM_STRAIGHT = 'smStraight';
+export const LG_STRAIGHT = 'lgStraight';
+export const KIND5 = 'kind5';
+export const CHANCE = 'chance';
+
 export const emptyScoreboard = {
-  ones: 0,
-  twos: 0,
-  threes: 0,
-  fours: 0,
-  fives: 0,
-  sixes: 0,
-  kind3: 0,
-  kind4: 0,
-  fullHouse: 0,
-  smStraight: 0,
-  lgStraight: 0,
-  kind5: 0,
-  chance: 0
+  [ONES]: 0,
+  [TWOS]: 0,
+  [THREES]: 0,
+  [FOURS]: 0,
+  [FIVES]: 0,
+  [SIXES]: 0,
+  [KIND3]: 0,
+  [KIND4]: 0,
+  [FULL_HOUSE]: 0,
+  [SM_STRAIGHT]: 0,
+  [LG_STRAIGHT]: 0,
+  [KIND5]: 0,
+  [CHANCE]: 0
 };
